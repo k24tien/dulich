fef399ff6d3e7dc53f092d856edd3638c01cf788
k24tien <kt7.2017.bl@gmail.com>
2018-01-06 08:40:07 +0700

    first commit

--- quantri/post_edit.php
+++ quantri/post_edit.php
@@ -11,7 +11,77 @@ if(isset($_GET['tt'])){
 }
 ?>
 <script>
-    
+    var tags = <?php echo json_encode($post['tags'])?>;
+    var arrayTags = new Array();
+    arrayTags = JSON.parse(tags);
+    $(function () {
+        $("#txtA_Title").focus();
+        $("#txtA_Title").keyup(function(){
+            assignTitleToAlias();
+        });
+        
+        $("#slA_Tinh").change(function(){
+            var data = {
+                hdTinh: $("#slA_Tinh").val()
+            };
+            $.ajax({
+                url:'chon_tinh_ajax.php',
+                type:'POST',
+                data:data,
+                success:function(data){
+                    console.log(data);
+                    $("#slA_Huyen").html(data);
+                },
+                error: function (data) {
+                    console.log(data);
+                }
+            });
+        });
+    });
+    function checkError(){
+        var txtA_Title = $('#txtA_Title');
+        var txtA_Alias = $('#txtA_Alias');
+        var slA_Category = $('#slA_Category');
+        var tagList = JSON.stringify(arrayTags);
+        $('#pTag').val(tagList);
+        var hasError = validateEmptyValue(new Array(txtA_Title, txtA_Alias, slA_Category));
+        if(hasError){
+            swal("Lỗi", "Vui long nhap day du thong tin", "error");
+            return false;
+        }
+
+    }
+    function setRedirect(){
+        $('#redirectPage').val(1);
+    }
+    function assignTitleToAlias(){
+        var title = $.trim($("#txtA_Title").val());
+        title = removeSigns(title);
+        $("#txtA_Alias").val(title);
+    }
+    function removeTag(key) {
+        // div contain key, name and value of property
+        var keySpan = $(key).parent();
+        // gỡ bỏ từ danh sách hiển thị
+        keySpan.remove();
+        // key and value
+        var keyValue = keySpan.attr('id');
+        // position of property to remove
+        var index = arrayTags.indexOf(keyValue);
+        // if exitst property in array then remove
+        if (index > -1) {
+            arrayTags.splice(index, 1);
+        }
+    }
+    function addTags(){
+        var nameTag = $("#txtA_Tag").val();
+        alert(tags);
+        if($.inArray(nameTag, arrayTags) == -1){
+            arrayTags.push(nameTag);
+            var key =  $('<div style="margin-left:5px; margin-bottom:5px" class="pull-left btn btn-xs btn-warning" id="' + nameTag + '">' + nameTag + '<i style="cursor: pointer;margin-left:5px" onclick="removeTag(this);" class="glyphicon glyphicon-remove-circle"></i></div>');
+            key.appendTo('#tagList');
+        }
+    }
 </script>
 <div class="d_divide20"></div>
 <div class="d_divide20"></div>
@@ -50,6 +120,7 @@ if(isset($_GET['tt'])){
                 </label>
                 <div class="col-sm-10">
                     <input type="text" class="form-control" title="" id="txtA_Title" name="txtA_Title" placeholder="Tên" value="<?php echo $post['title'];?>">
+                    <input type="hidden" title="" id="txtE_Title1" name="txtE_Title1" value="<?php echo $post['title'];?>">
                 </div>
             </div>
             <div class="form-group col-md-12 col-sm-12">
@@ -58,6 +129,7 @@ if(isset($_GET['tt'])){
                 </label>
                 <div class="col-sm-10">
                     <input type="text" class="form-control" title="" id="txtA_Alias" name="txtA_Alias" placeholder="Định danh" value="<?php echo $post['alias'];?>">
+
                 </div>
             </div>
             <div class="form-group col-md-12 col-sm-12">
@@ -113,7 +185,11 @@ if(isset($_GET['tt'])){
                     <select  class="form-control" name="slA_Tinh" id="slA_Tinh">
                         <option value="">-- Tỉnh/TP --</option>
                         <?php foreach($arrTinh as $tinh){?>
+                        <?php if($post['tinh']==$tinh){?>
+                        <option selected value="<?php echo $tinh; ?>"><?php echo $tinh; ?></option>
+                        <?php }else{?>
                         <option value="<?php echo $tinh; ?>"><?php echo $tinh; ?></option>
+                        <?php }?>
                         <?php }?>
                     </select>
                 </div>
@@ -121,9 +197,17 @@ if(isset($_GET['tt'])){
                     Quận huyện                      
                 </label>
                 <div class="col-sm-4">
-                    <select  class="form-control" name="slA_Huyen" id="slA_Huyen" disabled>
-                        <option value="">-- Quận huyện --</option>
+                    <select  class="form-control" name="slA_Huyen" id="slA_Huyen">
 
+                        <option value="">-- Quận huyện --</option>
+                        <?php $huyens = $arrQuanHuyen[$post['tinh']]?>
+                        <?php foreach($huyens as $huyen){?>
+                        <?php if($post['huyen']==$huyen){?>
+                        <option selected value="<?php echo $huyen; ?>"><?php echo $huyen; ?></option>
+                        <?php }else{?>
+                        <option value="<?php echo $huyen; ?>"><?php echo $huyen; ?></option>
+                        <?php }?>
+                        <?php }?>
                     </select>
                 </div>
             </div>
@@ -140,7 +224,9 @@ if(isset($_GET['tt'])){
                         </a>
                         
                         <div class="tagList" id="tagList">
-                            
+                            <?php foreach ($post['tags'] as $key => $value) {?>
+                               <div style="margin-left:5px; margin-bottom:5px" class="pull-left btn btn-xs btn-warning" id="<?php echo $value; ?>"><?php echo $value; ?><i style="cursor: pointer;margin-left:5px" onclick="removeTag(this);" class="glyphicon glyphicon-remove-circle"></i></div>
+                            <?php }?>
                         </div>
                         <input type="hidden" name="pTag" id="pTag" value="">
                     </div>
@@ -151,7 +237,7 @@ if(isset($_GET['tt'])){
                     Mô tả                          
                 </label>
                 <div class="col-sm-10">
-                    <textarea class="form-control" id="txtA_Des" name="txtA_Des"></textarea> 
+                    <textarea class="form-control" id="txtA_Des" name="txtA_Des"><?php echo $post['des']?></textarea> 
                 </div>
             </div>
             <div class="form-group col-md-12 col-sm-12">
@@ -159,7 +245,7 @@ if(isset($_GET['tt'])){
                     Nội dung                         
                 </label>
                 <div class="col-sm-10">
-                    <textarea class="form-control ckeditor" id="txtA_Content" name="txtA_Content"></textarea> 
+                    <textarea class="form-control ckeditor" id="txtA_Content" name="txtA_Content"><?php echo $post['content']?></textarea> 
                 </div>
             </div>
         
@@ -167,3 +253,47 @@ if(isset($_GET['tt'])){
 </div>
 </form>
 <?php include("footer.php");?>
+<?php 
+    if(isset($_POST['txtA_Title'])){
+        $id = $_POST['txtE_Title1'];
+        $redirectPage = $_POST['redirectPage'];
+        $title = $_POST['txtA_Title'];
+        $alias = $_POST['txtA_Alias'];
+        $filename = $_FILES["file"]["tmp_name"];
+        $category = $_POST['slA_Category'];
+        $urlmap = $_POST['txtA_Urlmap'];
+        $address = $_POST['txtA_Address'];
+        $tinh = $_POST['slA_Tinh'];
+        $huyen = $_POST['slA_Huyen'];
+        $des = $_POST['txtA_Des'];
+        $content = $_POST['txtA_Content'];
+        $tagList = json_decode($_POST['pTag']);
+        $collection = $db->post;
+        if($filename!=""){
+            $doc = array( "title" => $title, "alias"=>$alias,"post_image" => new MongoBinData(file_get_contents($filename)),"category" => $category, "urlmap" => $urlmap,"address" => $address, "tinh" => $tinh,"huyen" => $huyen, "des" => $des, "content" => $content, "tags"=>$tagList, "view" => 0,"like"=>0,"comment"=>array(), "created_date" => new MongoDate(), "author" => "Administrator");
+        }else{
+            $doc = array( "title" => $title, "alias"=>$alias,"category" => $category, "urlmap" => $urlmap,"address" => $address, "tinh" => $tinh,"huyen" => $huyen, "des" => $des, "content" => $content, "tags"=>$tagList, "view" => 0,"like"=>0,"comment"=>array(), "created_date" => new MongoDate(), "author" => "Administrator");
+        }
+        
+        $collection->update(array("title" => $id),$doc);
+        if($redirectPage==1){
+            echo "<script>
+            swal({
+                title: 'Thao tác thành công!',
+                timer: 10000,
+                type: 'success'
+            });
+            window.location.replace('http://localhost/doan/quantri/post.php');
+            </script>";  
+        }else{
+            echo "<script>
+            swal({
+                title: 'Thao tác thành công!',
+                timer: 10000,
+                type: 'success'
+            });
+            window.location.replace('http://localhost/doan/quantri/addNewPost.php');
+            </script>";  
+        }
+    }
+?>
